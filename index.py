b46a96c89bf0f1d1fac7c15f36bc6dac48f8722b
an1rxdh664 <anirudhhh637@gmail.com>
2025-11-04 22:55:06 +0530

    ...

---
+++ index.py
@@ -0,0 +1,24 @@
+name="Anirudh"
+age = 20
+# print(f"my name is {name} and i am {age} years old")
+
+word = "Python"
+# print(word[1::2])
+
+word = "J" + word[1:]
+# print(word)
+
+# upper lower both negative
+
+
+# name = "deepak"
+# name = f"{name[0]}a{name[2:]}" #string slicing
+# print(name)
+
+# name1 = "deepak"
+# name1 = name1.replace(name1[1], 'a') # replace
+# print(name1)
+
+
+ex_str = "   thirty Days of PYTHON  "
+print(dir(ex_str))
