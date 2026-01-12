6f76234484fc4fcf9c413497e7385a9259edf14c
an1rxdh664 <anirudhhh637@gmail.com>
2026-01-12 11:47:45 +0530

    lab-commit

---
+++ lab_notes.py
@@ -0,0 +1,10 @@
+# complex data - a = 5 + 2j
+# str = "3.5"
+# print(int(str)) -- valueError
+
+# operators are operations performed between operands using symbol.
+# // - float division
+
+# ~ -> adds one and converts the result into negative integer.
+# print(~2) # -> -3
+
