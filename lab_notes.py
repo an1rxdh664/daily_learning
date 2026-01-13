aef26fa5203fd89b03517a335be7a1124fbc1fc1
an1rxdh664 <anirudhhh637@gmail.com>
2026-01-13 23:12:37 +0530

    daily commit

--- lab_notes.py
+++ lab_notes.py
@@ -8,3 +8,20 @@
 # ~ -> adds one and converts the result into negative integer.
 # print(~2) # -> -3
 
+import random
+print(random.randrange(1,100))
+
+'''
+Program is a set of instructions
+Programming is a process of writing a task
+'''
+
+# membership operator - 'in' and 'not in'
+
+# identity operator - 'is' and 'is not'
+a = "Anirudh"
+b = 23
+c = "Bhadiya"
+
+# print("Hello {}, Your age : {}. Aur kaisa h? {}".format(a,b,c))
+print("Hello {1}, Your age : {2}. Aur kaisa h? {0}".format(c,a,b))
